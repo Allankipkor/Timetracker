e9be7f08c69d39e084a5af4d4c5879593d9e16f3
Allan Cheruiyot <allankipkorir68@gmail.com>
2026-07-08 23:45:25 +0300

    chore: change Android application name to TimeTracker

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.allan.timecamp',
-  appName: 'timecamp',
+  appName: 'TimeTracker',
   webDir: 'dist'
 };
 
